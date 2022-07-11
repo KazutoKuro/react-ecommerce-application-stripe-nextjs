d0e127b5a438e3172be788845dd6c2168fe5d510
KauztoKuro <ikkikurogane70@gmail.com>
2022-07-11 14:26:40 +0630

    Hook up Sanity

--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,36 @@ import React from "react";
 
 import { Product, FooterBanner, HeroBanner } from "../components";
 
-const Home = () => {
+import { client } from "../lib/client";
+
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner heroBanner = {bannerData.length && bannerData[0]} />
+      {console.log(bannerData)}
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
       <div className="products-container">
-        {["Product 1", "Product 2", "Product 3"].map((product) => product)}
+        {products?.map((product) => product.name)}
       </div>
       <FooterBanner />
     </>
   );
 };
 
+// In reactjs we use useEffect to fetch data, but in Nextjs, we use getServerSideProps whenever we want to fetch data
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
+};
 export default Home;
